02740a802c9423fc27dfedb06b0d992cbba03247
TheRealRondon <amirhossein2324236@gmail.com>
2021-08-20 22:57:49 +0430

    Added age.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from graphene import ObjectType, String, Schema
+from graphene import ObjectType, String, Schema, Int
 
 class Query(ObjectType):
-    hello = String(name=String(default_value="stranger"))
+    hello = String(name=String(default_value="stranger"),
+    age=Int(default_value=0))
     goodbye = String()
 
-    def resolve_hello(root, info, name):
-        return f'Hello {name}!'
+    def resolve_hello(root, info, name, age):
+        return f'Hello {name} with age {age}!'
 
     def resolve_goodbye(root, info):
         return 'See ya!'
